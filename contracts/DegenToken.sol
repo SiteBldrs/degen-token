25e23977ce461a337a233cc125910ce3f6632498
Abdullahi Salihu <sitebldrs@gmail.com>
2024-06-11 14:09:04 +0100

    get redeemed item

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,26 +6,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender){}
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
+
+    // mapping to store redeemed items for each user
+    mapping(address => string[]) private redeemedItems;
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    // 0xB6B0746f8137Db1E788597CFcD818e2B3bfF6324
-    // 100
 
-    function decimals() override public pure returns (uint8) {
+    function decimals() public pure override returns (uint8) {
         return 0;
     }
 
     function getBalance() external view returns (uint256) {
-        return this.balanceOf(msg.sender);
+        return balanceOf(msg.sender);
     }
 
     function transferTokens(address _receiver, uint256 _value) external {
         require(balanceOf(msg.sender) >= _value, "Not enough Token");
-        approve(msg.sender, _value);
-        transferFrom(msg.sender, _receiver, _value);
+        transfer(_receiver, _value);
     }
 
     function burnTokens(uint256 _value) external {
@@ -34,30 +34,31 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
     }
 
     function showStoreItems() external pure returns (string memory) {
-        return "The following items are available for purchase:\nSelection 1. Official Degen NFT\nSelection 2. Official Degen NFT\nSelection 3. Official Degen NFT";
+        return "The following items are available for purchase:\n1. Official Degen NFT - 100 Tokens\n2. Official Degen T-shirt - 75 Tokens\n3. Official Degen Sticker - 50 Tokens";
     }
 
-    function redeemTokens(uint8 _userChoice) external payable returns (bool) {
+    function redeemTokens(uint8 _userChoice) external returns (bool) {
         if (_userChoice == 1) {
-            require(this.balanceOf(msg.sender) >= 100, "Not enough Token");
-            approve(msg.sender, 100);
-            transferFrom(msg.sender, owner(), 100);
+            require(balanceOf(msg.sender) >= 100, "Not enough Token");
+            transfer(owner(), 100);
+            redeemedItems[msg.sender].push("Official Degen NFT");
             return true;
-        }
-        else if (_userChoice == 2) {
-            require(this.balanceOf(msg.sender) >= 75, "Not enough Token");
-            approve(msg.sender, 75);
-            transferFrom(msg.sender, owner(), 75);
+        } else if (_userChoice == 2) {
+            require(balanceOf(msg.sender) >= 75, "Not enough Token");
+            transfer(owner(), 75);
+            redeemedItems[msg.sender].push("Official Degen T-shirt");
             return true;
-        }
-        else if (_userChoice == 3) {
-            require(this.balanceOf(msg.sender) >= 50, "Not enough Token");
-            approve(msg.sender, 50);
-            transferFrom(msg.sender, owner(), 50);
+        } else if (_userChoice == 3) {
+            require(balanceOf(msg.sender) >= 50, "Not enough Token");
+            transfer(owner(), 50);
+            redeemedItems[msg.sender].push("Official Degen Sticker");
             return true;
-        }
-        else {
+        } else {
             return false;
         }
+    }
+
+    function getRedeemedItems() external view returns (string[] memory) {
+        return redeemedItems[msg.sender];
     }
 }
